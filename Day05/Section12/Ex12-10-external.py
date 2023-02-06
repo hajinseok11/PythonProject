ad3d5c6190a9400ff159b7258649ee33afa81a93
Jinseok <ha661001@nate.com>
2023-02-06 20:56:25 +0900

    Day06

---
+++ Day05/Section12/Ex12-10-external.py
@@ -0,0 +1,17 @@
+'''
+파일명 : Ex12-10-external.py
+
+패키지
+    모듈 상위 개념
+    모듈의 집합을 의미한다.
+
+패키지 설치
+console> pip install package 명
+
+패키지 삭제
+console> pip uninstall package 명
+'''
+# 행렬 연산 관련 package
+import numpy as np
+
+print(np.sum([1,2,3,4,5]))
